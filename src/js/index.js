016d434c58dfff51d7ea500aa06a95ae697bacbc
Jennifer Saivan <138686475+JenniferSaivan@users.noreply.github.com>
2023-07-10 12:49:19 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+
+// Esse projeto foi uma inspiração  
+
+
+
+const modal = document.querySelector('.modal');
+const botaoTrailer = document.querySelector('.botao-trailer');
+const botaoFecharModal = document.querySelector('.fechar-modal');
+const video = document.getElementById('video');
+const linkDoVideo = video.src;
+
+function alternarModal() {
+	modal.classList.toggle('aberto');
+}
+
+botaoTrailer.addEventListener('click', () => {
+	video.setAttribute('src', linkDoVideo);
+	alternarModal();
+});
+
+botaoFecharModal.addEventListener('click', () => {
+	video.setAttribute('src', '');
+	alternarModal();
+});
